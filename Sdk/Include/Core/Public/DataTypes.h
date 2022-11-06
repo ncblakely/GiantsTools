b31a72c7ad5264fce19d105980d918043cbb0604
Nick Blakely <ncblakely@gmail.com>
2022-11-05 17:36:54 -0700

    Create 1.5.1 branch

--- Sdk/Include/Core/Public/DataTypes.h
+++ Sdk/Include/Core/Public/DataTypes.h
@@ -5,6 +5,7 @@
 #include <memory>
 #include <string>
 #include <string_view>
+#include <array>
 
 //////////////////////////////////////////////////////////////////////////////////////
 // Basic game data types and macros
